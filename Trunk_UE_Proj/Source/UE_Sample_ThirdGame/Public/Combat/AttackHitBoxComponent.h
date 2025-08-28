84b8d98077f826ee32928e6f3b02aeb1239f53c5
TimliDowney <guojianda@spikewavegames.com>
2025-08-28 10:22:50 +0800

    add attack patch.

--- Trunk_UE_Proj/Source/UE_Sample_ThirdGame/Public/Combat/AttackHitBoxComponent.h
+++ Trunk_UE_Proj/Source/UE_Sample_ThirdGame/Public/Combat/AttackHitBoxComponent.h
@@ -3,32 +3,56 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/ActorComponent.h"
+#include "AttackData.h"
+#include "CombatTypes.h"
+#include "Components/BoxComponent.h"
 #include "AttackHitBoxComponent.generated.h"
 
 
+struct FAttackInfo;
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class UE_SAMPLE_THIRDGAME_API UAttackHitBoxComponent : public UActorComponent
+class UE_SAMPLE_THIRDGAME_API UAttackHitBoxComponent : public UBoxComponent
 {
 	GENERATED_BODY()
 
-
-	
 public:	
 	// Sets default values for this component's properties
 	UAttackHitBoxComponent();
 
-	
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="HitBox")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Instanced, Category="HitBox")
+	UAttackData* BaseAttackData;
 	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="HitBox")
+	FAttackInfo AttackInfo;
+	
+	UPROPERTY(Transient)
+	UAttackData* CurAttackData;
+	
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	UFUNCTION(BlueprintCallable, Category="HitBox")
+	void BeginAttackWindow(UAttackData* InData, FAttackInfo& InInfo);
+	
+	UFUNCTION(BlueprintCallable, Category="HitBox")
+	void EndAttackWindow();
 
+private:
 	
-	
+	UFUNCTION()
+	void OnHitBoxOverlap(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult);
 };
