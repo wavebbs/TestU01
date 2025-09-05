53f6d58064f295179d67754afe21ba496327a3d0
TimliDowney <guojianda@spikewavegames.com>
2025-09-05 18:12:24 +0800

    Components valid check.

--- Trunk_UE_Proj/Source/UE_Sample_ThirdGame/Characters/MonsterCharacter.cpp
+++ Trunk_UE_Proj/Source/UE_Sample_ThirdGame/Characters/MonsterCharacter.cpp
@@ -3,19 +3,33 @@
 
 #include "Characters/MonsterCharacter.h"
 
+#include "AIController.h"
 #include "Combat/CharacterBeHitLogicComponent.h"
 #include "Combat/CharacterDataComponent.h"
-#include "Combat/HurtBoxComponent.h"
 #include "Combat/HurtBoxHandlerComponent.h"
 
 /**
  * \brief 构造函数，创建并附加各组件
  */
 AMonsterCharacter::AMonsterCharacter()
-	: CharacterBeHitLogic(CreateDefaultSubobject<UCharacterBeHitLogicComponent>(TEXT("CharacterBeHitLogicComponent")))
-	, HurtBoxHandler(CreateDefaultSubobject<UHurtBoxHandlerComponent>(TEXT("HurtBoxHandlerComponent")))
-	, CharacterData(CreateDefaultSubobject<UCharacterDataComponent>(TEXT("CharacterDataComponent")))
-	//, HurtBoxComponent(CreateDefaultSubobject<UHurtBoxComponent>(TEXT("HurtBoxComponent")))
 {
 	// TODO
+	if (!CharacterBeHitLogic)
+	{
+		CharacterBeHitLogic = CreateDefaultSubobject<UCharacterBeHitLogicComponent>(TEXT("CharacterBeHitLogicComponent"));
+	}
+
+	// 检查并创建 HurtBoxHandlerComponent，避免重复创建
+	if (!HurtBoxHandler)
+	{
+		HurtBoxHandler = CreateDefaultSubobject<UHurtBoxHandlerComponent>(TEXT("HurtBoxHandlerComponent"));
+	}
+
+	// 检查并创建 CharacterDataComponent，避免重复创建
+	if (!CharacterData)
+	{
+		CharacterData = CreateDefaultSubobject<UCharacterDataComponent>(TEXT("CharacterDataComponent"));
+	}
+	
+	AIControllerClass = AAIController::StaticClass();
 }
